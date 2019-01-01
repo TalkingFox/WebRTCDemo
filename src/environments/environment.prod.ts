ffbd1fbd04cfaa1ddcc6687fffc9abe0e17bd265
Mike Wilkinson <mwilkinson@imo-online.com>
2018-12-31 18:02:44 -0600

    config inits

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  accessKey: '',
+  secretKey: '',
+  awsIot: '',
+  awsRegion: '',
+  signalServer: ''
 };
--- src/services/roomservice.service.ts
+++ src/services/roomservice.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { FoxConnect } from 'foxconnect';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomserviceService {
+  private foxConnect: FoxConnect;
 
-  constructor() { }
+  constructor() {
+    this.foxConnect = new FoxConnect({
+      awsAccessKey: environment.accessKey,
+      awsIotHost: environment.awsIot,
+      awsRegion: environment.awsRegion,
+      awsSecretKey: environment.secretKey,
+      signalServer: environment.signalServer,
+      clientId: `${Math.floor(Math.random() * 1000000 + 1)}`
+    });
+  }
 }
